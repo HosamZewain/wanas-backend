7da3306a9adcc3557f5a6342c37b9b7fb73339a3
khaled Al-wakeel <khaled_elwakeel@Outlook.com>
2021-12-26 13:17:38 +0200

    Update UserVehicleResource.php

--- app/Http/Resources/Api/UserVehicleResource.php
+++ app/Http/Resources/Api/UserVehicleResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\Api;
 
+use App\Models\UserVehicle;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -23,12 +24,13 @@ class UserVehicleResource extends JsonResource
             'image' => ($this->image) ? asset('storage/' . $this->image) : null,
             'type' => $this->type,
             'status' => $this->status,
+            'is_car_verified' => (bool)($this->status == UserVehicle::STATUS_APPROVED),
             'status_text' => $this->status_text,
             'attachments' => AttachmentResource::collection($this->whenLoaded('attachments')),
-            'car_license_front'=> ($this->car_license_front) ? asset('storage/' . $this->car_license_front) : null,
-            'car_license_back'=>  ($this->car_license_back) ? asset('storage/' . $this->car_license_back) : null,
-            'driver_license_front'=> ($this->driver_license_front) ? asset('storage/' . $this->driver_license_front) : null,
-            'driver_license_back'=>  ($this->driver_license_back) ? asset('storage/' . $this->driver_license_back) : null,
+            'car_license_front' => ($this->car_license_front) ? asset('storage/' . $this->car_license_front) : null,
+            'car_license_back' => ($this->car_license_back) ? asset('storage/' . $this->car_license_back) : null,
+            'driver_license_front' => ($this->driver_license_front) ? asset('storage/' . $this->driver_license_front) : null,
+            'driver_license_back' => ($this->driver_license_back) ? asset('storage/' . $this->driver_license_back) : null,
         ];
     }
 }
