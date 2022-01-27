e5651ea6f42d2954a5d474adf942a2df0a25a47b
khaled Al-wakeel <khaled_elwakeel@Outlook.com>
2022-01-27 22:41:04 +0200

    Update UserResource.php

--- app/Http/Resources/Api/UserResource.php
+++ app/Http/Resources/Api/UserResource.php
@@ -26,7 +26,7 @@ class UserResource extends JsonResource
             'status' => $this->status,
             'is_verified' => $this->is_verified,
             'gender' => $this->gender,
-            'rate' => $this->rate,
+            'rate' => round($this->rate, 2),
             $this->mergeWhen($this->country, [
                 'country' => $this->country,
             ]),
